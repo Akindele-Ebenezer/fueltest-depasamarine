6e61651b170a61527246809f6afd9c5e62f41268
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-15 12:49:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300&family=Open+Sans&family=Rowdies:wght@300&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="styles/styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FUEL TEST</title>
+</head>
+<body>
+    
+    <form action="">
+        <div class="form">
+            <div class="form-info-wrapper">
+                <h1>DEPASA Marine Int'l</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, sint.</p>
+                <div class="form-body-wrapper">
+                    <div class="contact">
+                        <div><img src="" alt="">  IMG</div>
+                        <div>Address</div>
+                    </div> 
+                    <div class="contact">
+                        <div><img src="" alt="">  IMG</div>
+                        <div>Phone</div>
+                    </div> 
+                    <div class="contact">
+                        <div><img src="" alt="">  IMG</div>
+                        <div>Email</div>
+                    </div> 
+                    <div class="contact">
+                        <div><img src="" alt="">  IMG</div>
+                        <div>Website</div>
+                    </div> 
+                </div>
+            </div>
+            <div class="input-container">
+                <h1>FUEL TEST</h1>
+                <div class="input-wrapper">
+                    <div>
+                        <label for="sample_number">Sample No.</label><br>
+                        <input type="number" placeholder="Enter No.">
+                    </div>
+
+                    <div>
+                        <label for="sample_collection_date">Sample Collection Date</label> <br>
+                        <input type="date" placeholder="date...">
+                    </div>
+
+                    <div>
+                        <label for="truck_plate_no">Truck Plate No.</label><br>
+                        <input type="text" placeholder="Enter Plate No.">
+                    </div>
+
+                    <div>
+                        <label for="tank_no">Tank No.</label><br>
+                        <input type="number" placeholder="Input No.">
+                    </div>
+
+                    <div>
+                        <label for="apperance_result">Apperance Result</label><br>
+                        <input type="radio" placeholder="Pick an Option...">
+                    </div>
+
+                    <div>
+                        <label for="color">Color</label><br>
+                        <input type="checkbox" placeholder="Pick a Color...">
+                    </div>
+
+                    <div>
+                        <label for="density">Density at 27° C in Kg/l</label><br>
+                        <input type="text" placeholder="Input Density...">
+                    </div>
+
+                    <div>
+                        <label for="flash_point">Flash Point</label><br>
+                        <input type="text" placeholder="Enter Flash Point...">
+                    </div>
+
+                    <div>
+                        <label for="temp">Temp</label><br>
+                        <input type="text" placeholder="Temperature...">
+                    </div>
+
+                    <div>
+                        <label for="water">Water ASTM D2709-16</label><br>
+                        <input type="text" placeholder="Required...">
+                    </div>
+
+                    <div>
+                        <label for="cleanliness">Cleanliness</label><br>
+                        <input type="text" placeholder="Cleanliness...">
+                    </div>
+
+                    <div>
+                        <label for="date_of_test">Date Of Test</label><br>
+                        <input type="date" placeholder="Date...">
+                    </div>
+
+                    <div>
+                        <label for="full_name">Made By (Name)</label><br>
+                        <input type="text" placeholder="Full Name...">
+                    </div>
+
+                </div> 
+                <br>
+                <input type="button" value="Submit">
+            </div>
+        </div>
+    </form>
+    
+
+</body>
+</html>
