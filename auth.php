408b8778ea3cc2ceb8534a1f0415ba4c2588f88f
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-22 19:39:27 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -7,9 +7,9 @@
     $query1 = mysqli_query($conn, $sql1);
     $result1 = mysqli_fetch_all($query1, MYSQLI_ASSOC); 
 
-    $_SESSION["full_name"] = $result1[0]["full_name"]; 
-    $full_name = $_SESSION["full_name"];
     $id = $_SESSION['id'];
+    $_SESSION["full_name"] = $result1[$id]["full_name"]; 
+    $full_name = $_SESSION["full_name"];
     
     if(!isset($_SESSION['auth'])){
         header("location: login.php");
