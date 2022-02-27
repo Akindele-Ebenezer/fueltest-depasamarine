c478a86557d91d36b7ad3476a23240ba3918f19a
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-27 22:41:09 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('localhost', 'root', 'ebenkeyz1*', 'fuel_test_management_system');
+    $conn = mysqli_connect('localhost', 'root', '', 'fuel_test_management_system');
 
     // $conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b22f0c6b8de0ae', 'a2daf15e', 'heroku_ffdd30cc455a32b');
 
