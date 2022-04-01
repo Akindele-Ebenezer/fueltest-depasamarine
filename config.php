8d65385f6d7d3f989b899032ff5d42aec2af0de3
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-04-01 17:03:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('localhost', 'seyi', 'seyi', 'fuel-test-lab');
+    $conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'b22f0c6b8de0ae', 'a2daf15e', 'heroku_ffdd30cc455a32b');
+//     $conn = mysqli_connect('localhost', 'seyi', 'seyi', 'fuel-test-lab');
 
 ?>
